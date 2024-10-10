f46939aff4ecfa74dd1800c634b0c6e0c356cc0e
misartain <misartai@purdue.edu>
2024-10-10 19:37:20 -0400

    updated link to css

--- my-react-app/src/pages/messaging.js
+++ my-react-app/src/pages/messaging.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './messaging/messages.css';
 
 const MessagePage = () => {
     return (
