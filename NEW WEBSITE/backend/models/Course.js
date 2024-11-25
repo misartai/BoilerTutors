f324cfcceaed1485549fcfcaafa5511f561135a1
aryanshahu <111910120+aryanshahu@users.noreply.github.com>
2024-11-24 23:14:50 -0500

    Update Course.js

--- NEW WEBSITE/backend/models/Course.js
+++ NEW WEBSITE/backend/models/Course.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
 
+const courseSchema = new mongoose.Schema({
+  courseName: { type: String, required: true },
+  courseDescription: { type: String, required: true },
+  announcements: { type: [String], default: [] },
+  professors: { type: [mongoose.Schema.Types.ObjectId], ref: 'User', required: true },
+  students: { type: [mongoose.Schema.Types.ObjectId], ref: 'User', default: [] },
+  tutors: { type: [mongoose.Schema.Types.ObjectId], ref: 'User', default: [] },
+});
+
+module.exports = mongoose.model('Course', courseSchema);
